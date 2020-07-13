8e009985bd36c9093aa238650d6cdbcb023a7bd2
SrBenja007 <suprmeobenjamin.cia@gmail.com>
2020-07-13 00:57:29 -0400

    Update

--- Bukkit/src/main/java/me/supremeproject/SupremeText.java
+++ Bukkit/src/main/java/me/supremeproject/SupremeText.java
@@ -1,5 +1,14 @@
 package me.supremeproject;
 
+import me.supremeproject.message.Message;
+import org.bukkit.entity.Player;
+
 public interface SupremeText {
+
+    void sendMessage(Player player, Message message);
+
+    void setTitle(Player player, String title, String subtitle, int time1, int time2, int time3);
+
+    void setActionBar(Player player, String text);
 
 }
