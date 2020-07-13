5fcb9434e368744c11c4b70bf83c5b36da51c5c4
SrBenja007 <suprmeobenjamin.cia@gmail.com>
2020-07-13 12:19:31 -0400

    Update

--- Bukkit/src/main/java/me/supremeproject/TextAPI.java
+++ Bukkit/src/main/java/me/supremeproject/TextAPI.java
@@ -7,12 +7,16 @@ public class TextAPI {
     public static SupremeText getAPI(){
         String version = Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3];
 
-        if (version.contains("1_8_R3")) {
-            return new Version_1_8_R3();
+        if(version.contains("1_8_R1")){
+            return new Version_1_8_R1();
         }
 
         if(version.contains("1_8_R2")){
             return new Version_1_8_R2();
+        }
+
+        if (version.contains("1_8_R3")) {
+            return new Version_1_8_R3();
         }
 
         return null;
